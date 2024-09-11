7ee3baa59c53ca8e4d19e91bca46ea4c5ed93b17
Monyealogan <monyealogan12@gmail.com>
2024-09-11 17:03:19 -0400

    Completed initial setup of application- from first video guide

--- app/src/main/java/com/example/d308_application/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308_application/UI/VacationDetails.java
@@ -2,53 +2,36 @@ package com.example.d308_application.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308_application.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class VacationList extends AppCompatActivity {
+public class VacationDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_vacation_list);
+        setContentView(R.layout.activity_vacation_details);
         //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             //Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+           // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             //return insets;
         FloatingActionButton fab=findViewById(R.id.floatingActionButton2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(VacationList.this, VacationDetails.class);
+                Intent intent=new Intent(VacationDetails.this, ExcursionDetails.class);
                 startActivity(intent);
             }
         });
         System.out.println(getIntent().getStringExtra("test"));
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.mysample) {
-            Toast.makeText(VacationList.this,"put in sample data", Toast.LENGTH_LONG).show();
-            return true;
-        }
-        if(item.getItemId()==android.R.id.home){
-            this.finish();
-            return true;
-        }
-        return true;
     }
 }
